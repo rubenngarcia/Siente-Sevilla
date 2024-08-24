b132bdfa24ec9164c5b1864faf01dcaa4038e44a
rubenngarcia <rgarciacarrillo96@gmail.com>
2024-08-24 11:07:56 +0200

    first commit

---
+++ src/main/java/com/sientesevilla/reservas_eventos/ReservasEventosApplication.java
@@ -0,0 +1,13 @@
+package com.sientesevilla.reservas_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sientesevilla/reservas_eventos/ReservasEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sientesevilla.reservas_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
